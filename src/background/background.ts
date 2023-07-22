d23c78cb828b2eafdbf6df47e000684b117b5f2f
Vahe Yeranosyan <vahe.yeranosian@gmail.com>
2023-07-23 01:33:17 +0400

    added action clicked for activate content parts

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,5 +1,35 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    console.log(msg);
-    console.log(sender);
-    sendResponse("Front the background Script");
-})
+// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+//     console.log(msg);
+//     console.log(sender);
+//     sendResponse("Front the background Script");
+// })
+
+
+let isActive = false; // Initial state of the extension icon
+
+chrome.action.onClicked.addListener(function (tab) {
+    isActive = !isActive; // Toggle the state on each click
+
+    const iconPath = isActive
+        ? {
+            "16": "icon_16active.png",
+            "32": "icon_32active.png",
+            "48": "icon_48active.png",
+            "128": "icon_128active.png"
+        }
+        : {
+            "16": "icon_16.png",
+            "32": "icon_32.png",
+            "48": "icon_48.png",
+            "128": "icon_128.png"
+        };
+
+    // Set the new icon based on the current state
+    chrome.action.setIcon({ path: iconPath });
+
+    // Send a message to the content script to perform actions
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const tabId = tabs[0].id;
+        chrome.tabs.sendMessage(tabId, { action: "toggleState", isActive: isActive });
+    });
+});
--- src/contentScript/index.tsx
+++ src/contentScript/index.tsx
@@ -5,6 +5,7 @@ import ContentScript from "./contentScript";
 
 function init() {
     const appContainer = document.createElement('div')
+    appContainer.id = "_content"
     document.body.appendChild(appContainer)
     if (!appContainer) {
         throw new Error("Can not find AppContainer");
@@ -13,8 +14,28 @@ function init() {
     // console.log(appContainer)
     root.render(<ContentScript />);
 }
+// Function to remove the target div element
+function removeTarget() {
+    const targetDiv = document.getElementById('_content'); // Replace 'your-div-id' with the actual id of the div you want to remove
 
-init();
+    if (targetDiv) {
+        targetDiv.remove();
+    }
+}
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === "toggleState") {
+        const isActive = message.isActive;
+        console.log("Content script activated with isActive = " + isActive);
+        if(isActive==true){
+            init();
+        }else {
+            removeTarget()
+        }
+
+        // Your actions based on the state change go here
+    }
+});
 
 
 // chrome.runtime.sendMessage('I am loading content script', (response) => {
